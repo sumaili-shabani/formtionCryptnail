34d0b8916e6c94ffa55d82059c349e892530ef19
sumaili-shabani <sumailiroger681@gmail.com>
2021-10-25 17:01:40 +0200

    introduction bootstrap

--- index.php
+++ index.php
@@ -1,47 +1,73 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>L'accueil vous soit doux!</title>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap-4.4.1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <!-- hors connexion -->
+
+    <!-- fontawason -->
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome/css/font-awesome.min.css">
+  	<link href="assets/css/google-font.css" rel="stylesheet">
+
+  	 <link rel="shortcut icon" href="assets/images/bootstrap.jpg">
+
+    <title>Accuel</title>
+  </head>
+  <body>
+
+  	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	  <a class="navbar-brand" href="#">
+	  	<font class="text-warning">Crypt</font><font class="text-dark">nail</font>
+	  </a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+
+	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    <ul class="navbar-nav ml-auto">
+	      <li class="nav-item active">
+	        <a class="nav-link" href="index.php"><i class="fa fa-home mr-1"></i>Accueil <span class="sr-only"></span></a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="blog.php"><i class="fa fa-tag mr-1"></i>Blog</a>
+	      </li>
+	      <li class="nav-item dropdown">
+	        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	          <i class="fa fa-cog mr-1"></i>Paramètre
+	        </a>
+	        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+	          <a class="dropdown-item" href="#"><i class="fa fa-camera mr-1"></i>Images</a>
+	          <a class="dropdown-item" href="#"><i class="fa fa-file-video-o mr-1"></i>Videos</a>
+	          <div class="dropdown-divider"></div>
+	          <a class="dropdown-item" href="#"><i class="fa fa-book mr-1"></i>Cours</a>
+	        </div>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="contact.php" tabindex="-1" aria-disabled="true"><i class="fa fa-envelope mr-1"></i>Contact</a>
+	      </li>
+	    </ul>
+	    <form class="form-inline my-2 my-lg-0">
+	      <input class="form-control mr-sm-2" type="search" placeholder="Recherche..." aria-label="Recherche...">
+	      <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fa fa-search mr-1"></i>Recherche...</button>
+	    </form>
+	  </div>
+	</nav>
 
 
-</head>
+
+    <h1>Bonjour on est prêt pour affronter Bootstrap!</h1>
 
 
-	<div class="menu">
-		<div id="menu_ok">
-			<a href="index.php" style="color: white; margin-right: 10px; margin-top: 40px;">Accueil</a> 
 
-			<a href="blog.php" style="color: white; margin-right: 10px; margin-top: 40px;">Blog</a> 
-
-			<a href="contact.php" style="color: white; margin-right: 10px; margin-top: 40px;">Contact</a> 
-			<a href="set.php" style="color: white; margin-right: 10px; margin-top: 40px;">Paramètre</a> 
-			<a href="form.php" style="color: white; margin-right: 10px; margin-top: 40px;">Forme</a> 
-		</div>
-	</div>
-
-	
-	<p>
-		<img src="assets/images/testimonials-3.jpg">
-
-		<span>
-			“Dans un monde plein de conflits, d’injustice, de pauvreté, et de désastre, nous voulons contribuer à des signes d’espoir et de restauration. «SANTE PLUS» est convaincue que le développement social, doit être d’abord une prise de conscience durable de la population Rd congolaise qui sa première richesse passe par la prise en compte de sa santé physique, et sa terre arable fertile en abondance et rare partout dans le monde!  Que l’exploitation de la terre par des individus en bonne santé est une voie sûre qui procure rapidement des ressources, un confort  de vie et une assurance pour l’avenir plus meilleurs.
-		</span>
-	</p>
-
-	<p> 
-		<h1>
-			<p>
-				Nous ici pour les voisins qui souffrent
-			</p>
-		</h1> 
-		<h4>Dans ce monde brisé</h4>
-	</p>
-
-
-	
-
-
-</body>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/css/bootstrap-4.4.1-dist/js/bootstrap.min.js"></script>
+  </body>
 </html>
